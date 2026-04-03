11d3eea286af241ea652ed75de0c800ffc95a090
Pen Guen <usual-blitz-bulge@duck.com>
2026-04-02 23:21:03 -0400

    Fix main page logo

--- components/MBMTLogo.tsx
+++ components/MBMTLogo.tsx
@@ -1,10 +1,10 @@
 export default function MBMTLogo() {
     return (
         <div className="text-[20vw] md:text-[10vw] md:px-[.2em] font-slabo select-none">
-            <span className="font-medium bg-theme text-[lightgray] leading-[1.1] px-[.2em] rounded-tl-[.2em] rounded-bl-[.2em]">
+            <span className="font-medium bg-theme dark:bg-theme-other text-[lightgray] leading-[1.1] px-[.2em] rounded-tl-[.2em] rounded-bl-[.2em]">
                 MB
             </span>
-            <span className="font-medium bg-[lightgray] text-theme leading-[1.1] px-[.2em] rounded-tr-[.2em] rounded-br-[.2em]">
+            <span className="font-medium bg-[lightgray] text-theme dark:text-theme-other leading-[1.1] px-[.2em] rounded-tr-[.2em] rounded-br-[.2em]">
                 MT
             </span>
         </div>
